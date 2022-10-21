5903a0c6b5145b1f1d2408290879eb390557817d
Sarah Hosni <92678234+delight04@users.noreply.github.com>
2022-10-21 20:26:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomImageSrc="images/dice" +randomNumber1+ ".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSrc);
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImageSrc2="images/dice" +randomNumber2+ ".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSrc2);
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🚩 Player 1 wins!";
+}
+else if (randomNumber2>randomNumber1) {
+  document.querySelector("h1").innerHTML="Player 2 wins! 🚩";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw";
+}
